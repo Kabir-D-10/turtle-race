738270b38f5ee4c1691bbd5416381dc98f855cf3
Kabir-D-10 <kabirdhadwal10@gmail.com>
2024-09-12 17:55:45 +0100

    Add files via upload

--- names dictionary.py
+++ names dictionary.py
@@ -1,9 +1,37 @@
-names = {
-    "Me":"Kabir",
-    "You":"Danstan"
+# names = {
+#     "Me":"Kabir",
+#     "You":"Danstan"
+# }
+# print(names.get("Me"))
+# print(names["You"])
+# names["You"]="Dalip"
+# names["Him"]="Danstan"
+# print(names)
+
+
+
+
+
+
+friends = {
+    "a":"red",
+    "b":"green",
+    "c":"blue"
 }
-print(names.get("Me"))
-print(names["You"])
-names["You"]="Dalip"
-names["Him"]="Danstan"
-print(names)
+# print(friends["a"])
+# friends["b"] = "orange"
+# print(friends["b"])
+# friends["d"] = "purple"
+# print(friends["d"])
+# friends.pop("a")
+# friends.popitem()
+# print(friends)
+# friends.clear()
+# print(friends)
+# del friends
+for key in friends.keys():
+    print(key)
+for val in friends.values():
+    print(val)
+for item in friends.items():
+    print(item)
